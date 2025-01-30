ca2d5c1566d586a4f873eae45ebff806a8821509
uttamkn <uttamkn15@gmail.com>
2025-01-30 14:25:01 +0530

    fix: change title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ export async function Navbar() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <Link href="/" className="text-xl font-bold text-primary">
-            CodeQuest
+            QRcode
           </Link>
 
           <div className="hidden md:flex space-x-4">
